d16129e9bd11c5fdb00e43dbda14d92139e3cd61
lucianolima2905 <65465300+lucianolima2905@users.noreply.github.com>
2023-10-23 18:24:34 -0300

    748

--- install/backup.sh
+++ install/backup.sh
@@ -5,6 +5,20 @@ if [ -f "/root/usuarios.db" ]; then
 	echo -e "Certificado Ovpn localizado..."
 	rm -rf $HOME/backup.vps >/dev/null 2>&1
 	sleep 1
+	tar cvf /root/backup.vps /root/usuarios.db /etc/shadow /etc/passwd /etc/group /etc/gshadow /etc/SSHPlus/senha /etc/SSHPlus/bot-telegram /etc/v2ray/config.json /etc/v2ray/domain.txt /etc/v2ray/v2ray.crt /etc/v2ray/v2ray.key >/dev/null 2>&1
+	echo ""
+	echo "BACKUP CRIADO COM SUCESSO !"
+	echo ""
+	} || {
+	rm -rf $HOME/backup.vps >/dev/null 2>&1
+	sleep 1
+	tar cvf /root/backup.vps /root/usuarios.db /etc/shadow /etc/passwd /etc/group /etc/gshadow /etc/SSHPlus/senha /etc/SSHPlus/bot-telegram >/dev/null 2>&1
+	echo ""
+	echo "BACKUP CRIADO COM SUCESSO !"
+	echo ""
+	} || {
+	rm -rf $HOME/backup.vps >/dev/null 2>&1
+	sleep 1
 	tar cvf /root/backup.vps /root/usuarios.db /etc/shadow /etc/passwd /etc/group /etc/gshadow /etc/SSHPlus/senha /etc/SSHPlus/v2ray /etc/openvpn $HOME/BOT/permitidos $HOME/BOT/revenda >/dev/null 2>&1
 	echo ""
 	echo "BACKUP CRIADO COM SUCESSO !"
